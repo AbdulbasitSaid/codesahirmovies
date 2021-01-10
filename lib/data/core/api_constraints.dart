85978df863055d69748dd6c17fc3bf84c234ada8
Abdulbasit <abdulbasitsaeed619@gmail.com>
2021-01-10 03:50:21 +0100

    api constraints

---
+++ lib/data/core/api_constraints.dart
@@ -0,0 +1,7 @@
+class ApiConstraints{
+  ApiConstraints._();
+
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = 'd9bfa050e55fc7066a126e192ace790f';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+}
